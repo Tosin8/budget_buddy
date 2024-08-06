2570d805e88aabfd5c97f23026ade5451cc7812c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-06 22:54:34 +0100

    Create Home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:budget_buddy/screens/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,10 +16,17 @@ class MyApp extends StatelessWidget {
       title: 'Expense Tracker',
       theme: ThemeData(
         
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+       // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+       colorScheme: ColorScheme.light(
+        surface: Colors.grey.shade100, // background
+        onSurface: Colors.black,  // onBackground
+        primary: const Color(0xff00b2e7), 
+        secondary: const Color(0xffe064f7), 
+        tertiary: const Color(0x0fff8d6c), 
+       ),
         useMaterial3: true,
       ),
-      home: const MyApp(), 
+      home: const HomeScreen(), 
     
     );
   }
