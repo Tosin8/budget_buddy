d6d2e8ac953b42bfa876927408c28e84124ec1d7
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-10 15:29:13 +0100

    adding layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:budget_buddy/screens/home/views/home.dart';
 import 'package:flutter/material.dart';
 
+import 'screens/layout/layout.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,24 +13,32 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Expense Tracker',
-      theme: ThemeData(
-        
-       // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-       colorScheme: ColorScheme.light(
-        surface: Colors.grey.shade100, // background
-        onSurface: Colors.black,  // onBackground
-        primary: const Color(0xff00b2e7), 
-        secondary: const Color(0xffe064f7), 
-        tertiary: const Color(0x0fff8d6c), 
-        outline: Colors.grey, 
-       ),
-        useMaterial3: true,
+    return  ScreenUtilInit(
+      designSize: const Size(360, 690), // Base size for calculations
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Expense Tracker',
+        theme: ThemeData(
+          
+         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+         colorScheme: ColorScheme.light(
+          surface: Colors.grey.shade100, // background
+          onSurface: Colors.black,  // onBackground
+          primary: const Color(0xff00b2e7), 
+          secondary: const Color(0xffe064f7), 
+          tertiary: const Color(0x0fff8d6c), 
+          outline: Colors.grey, 
+         ),
+          useMaterial3: true,
+        ),
+        home: const Layout(), 
+      
       ),
-      home: const HomeScreen(), 
-    
     );
-  }
+  }, 
+    );
+}
 }
