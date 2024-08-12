a9475380d085092513e316b77fd5cab0af926ffc
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-12 14:27:59 +0100

    Update home.dart

--- lib/screens/layout/mobile/home.dart
+++ lib/screens/layout/mobile/home.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MobileHome extends StatelessWidget {
   const MobileHome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
-        children: [],
+        mainAxisAlignment: MainAxisAlignment.center, 
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: double.infinity, 
+            height: 300.h,
+            child: Image.asset(
+           ' assets/images/not_found.jpg'),
+            ),
+            SizedBox(height: 30.h,), 
+            const Text('Mobile View for Budget Buddy is currently not avaiblabe on Mobile View, Kindly install it on tablet or visit the MS Store for Desktop mode',), 
+            SizedBox(height: 40.h,), 
+      
+        ],
       )
     );
   }
