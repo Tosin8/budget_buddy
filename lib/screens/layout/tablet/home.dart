25ff3a1f245909a2be850e13e66997e49ee35a31
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-27 23:50:02 +0100

    Update home.dart

--- lib/screens/layout/tablet/home.dart
+++ lib/screens/layout/tablet/home.dart
@@ -8,38 +8,53 @@ class TabletView extends StatefulWidget {
 }
 
 class _TabletViewState extends State<TabletView> {
+
+  late PageController _pageController; 
+  int totalPage = 4; 
+
+  void _onScroll() {
+
+}
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 0)..addListener(_onScroll); 
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold( 
-      body: PageView(
-        children:  [
-          Container(
-            decoration: const BoxDecoration(), 
-            child:  Column(
-              children: [
-// Image
-                Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/icons/track_expenses.jpg'))
-                ),
-                ),
-
-                // title
-                const Text('Smart Spending'), 
-                
-                //sub title.  
-                const Text('Organize expenses and streamline your finances')
-              ],
-            )
-          ), 
-Container(
-            decoration: const BoxDecoration(color: Colors.green),
-          ), Container(
-            decoration: const BoxDecoration(color: Colors.pink),
-          ), 
-        ],
-      )
+      body: Stack(
+        children:[ PageView(
+          controller: _pageController,
+          children:  [
+            Container(
+              decoration: const BoxDecoration(), 
+              child:  Column(
+                children: [
+        // Image
+                  Container(
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/icons/track_expenses.jpg'))
+                  ),
+                  ),
+        
+                  // title
+                  const Text('Smart Spending'), 
+                  
+                  //sub title.  
+                  const Text('Organize expenses and streamline your finances')
+                ],
+              )
+            ), 
+        Container(
+              decoration: const BoxDecoration(color: Colors.green),
+            ), Container(
+              decoration: const BoxDecoration(color: Colors.pink),
+            ), 
+          ],
+        ),
+     ] )
     );
   }
 }
